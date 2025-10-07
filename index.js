04fed1b4a29eab6fbca5db654a0ef79ca15e3a6b
Amar Shukla <amarshukla@Amars-MacBook-Air.local>
2025-10-08 00:34:19 +0530

    feat: Added hotel

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
 
 app.use(express.json())
+app.use(cors())
 
 const { initializeDB } = require("./db/db.connect")
 const Hotel = require("./models/hotel.models")
